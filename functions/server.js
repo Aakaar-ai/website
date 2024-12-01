c237a815ec23feb6d103d7ece178c452d09d0106
madguy02-blip <manish@aakaarai.com>
2024-12-01 22:17:34 +0530

    feat(website): debug logs 6

--- functions/server.js
+++ functions/server.js
@@ -26,7 +26,7 @@ require('dotenv').config();
              code: code,
              client_id: process.env.CLIENT_ID,
              client_secret: process.env.CLIENT_SECRET,
-             redirect_uri: 'https://aakaarai.com/amazon',
+             redirect_uri: 'https://www.aakaarai.com/amazon',
            }),
            {
              headers: {
